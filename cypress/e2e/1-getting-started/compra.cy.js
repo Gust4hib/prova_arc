d57aa1f5eb7ce421f22e94505a23947dfb0db9c5
Gustavo <gust4vof2p@gmail.com>
2025-07-18 20:31:36 -0400

    Corrige testes: login.cy.js agora contém cenários de login

--- cypress/e2e/1-getting-started/compra.cy.js
+++ cypress/e2e/1-getting-started/compra.cy.js
@@ -1,22 +1,13 @@
-// cypress/e2e/1-getting-started/compra-saucedemo.cy.js
-
 describe('Testes de Funcionalidade Pós-Login - Sauce Demo', () => {
-
-    // Pré-condição para todos os testes neste arquivo: realizar login com sucesso
     beforeEach(() => {
-        // PASSO 1: Visita a página de login
         cy.visit('https://www.saucedemo.com/');
-
-        // PASSO 2: Realiza o login usando o comando personalizado
         cy.login('standard_user', 'secret_sauce');
-
-        // PASSO 3: Confirma que o login foi bem-sucedido e estamos na página de produtos
         cy.url().should('include', '/inventory.html');
         cy.get('.title').should('contain', 'Products');
     });
 
     it('COMPRA_001: Deve adicionar um item ao carrinho de compras e verificar sua presença', () => {
-        const productName = 'Sauce Labs Backpack'; // Item a ser adicionado
+        const productName = 'Sauce Labs Backpack';
 
         cy.adicionarItemAoCarrinho(productName);
 
@@ -25,6 +16,7 @@ describe('Testes de Funcionalidade Pós-Login - Sauce Demo', () => {
         cy.get('.shopping_cart_link').click();
 
         cy.url().should('include', '/cart.html');
+        
         cy.contains('.inventory_item_name', productName).should('be.visible');
     });
 
--- cypress/e2e/1-getting-started/login.cy.js
+++ cypress/e2e/1-getting-started/login.cy.js
@@ -1,30 +1,33 @@
-// cypress/e2e/1-getting-started/compra-saucedemo.cy.js
+describe('Testes de Sanidade na Página de Login - Sauce Demo', () => {
 
-describe('Testes de Funcionalidade Pós-Login - Sauce Demo', () => {
-
-    // Pré-condição para todos os testes neste arquivo: realizar login com sucesso
     beforeEach(() => {
         cy.visit('https://www.saucedemo.com/');
-        cy.login('standard_user', 'secret_sauce'); // Realiza o login usando o comando
-        cy.url().should('include', '/inventory.html'); // Confirma que está na página de produtos
-        cy.get('.title').should('contain', 'Products'); // Confirma o título da página
     });
 
-    it('COMPRA_001: Deve adicionar um item ao carrinho de compras e verificar sua presença', () => {
-        const productName = 'Sauce Labs Backpack'; // Item a ser adicionado
+    it('LOGIN_001: Deve realizar login com sucesso usando credenciais válidas (standard_user)', () => {
+        cy.login('standard_user', 'secret_sauce');
 
-        // Usa o comando personalizado 'adicionarItemAoCarrinho'
-        cy.adicionarItemAoCarrinho(productName);
+        cy.url().should('include', '/inventory.html');
+        cy.get('.title').should('contain', 'Products');
+    });
 
-        // Asserções para verificar a adição ao carrinho
-        cy.get('.shopping_cart_badge').should('contain', '1'); // Verifica o contador do carrinho
+    it('LOGIN_002: Deve exibir mensagem de erro para usuário inválido', () => {
+        cy.login('usuario_invalido', 'secret_sauce');
 
-        // Navega para o carrinho de compras
-        cy.get('.shopping_cart_link').click();
+        cy.get('[data-test="error"]').should('contain', 'Epic sadface: Username and password do not match any user in this service');
+        cy.url().should('eq', 'https://www.saucedemo.com/');
+    });
 
-        // Asserções para verificar o item no carrinho
-        cy.url().should('include', '/cart.html'); // Verifica se a URL é a do carrinho
-        cy.contains('.inventory_item_name', productName).should('be.visible'); // Verifica se o item está visível no carrinho
+    it('LOGIN_003: Deve exibir mensagem de erro para senha inválida', () => {
+        cy.login('standard_user', 'senha_invalida');
+        cy.get('[data-test="error"]').should('contain', 'Epic sadface: Username and password do not match any user in this service');
+        cy.url().should('eq', 'https://www.saucedemo.com/');
+    });
+
+    it('LOGIN_004: Deve exibir mensagem de erro para usuário bloqueado (locked_out_user)', () => {
+        cy.login('locked_out_user', 'secret_sauce');
+        cy.get('[data-test="error"]').should('contain', 'Epic sadface: Sorry, this user has been locked out.');
+        cy.url().should('eq', 'https://www.saucedemo.com/');
     });
 
 });
